850cb82f2bfbe01ddaf2367fddfffb33fb798e31
RamonaEJSpence <ramonaejspence@gmail.com>
2020-03-01 10:45:20 -0500

    added static files, and edited polls/admin.py to include a model admin class (lines 5 and 6), now I'm going to change it to use tuples in the fields attribute

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+## class changes the admin options for a model, in this case Question
+
+admin.site.register(Question, QuestionAdmin)
+##adding QuestionAdmin as the second argument to Question,
+##let's us access these fields we've provided in QuestionAdmin
+
+## NOTE: this only changes the order of the fields as the appear in the admin page
+## NOTE: this let's us define much more detailed/numerous fields for admin page
